2a50b24f696ac75ecc51594facda8c79d03aaecf
Marjorie Wuckert <mwuckert@e4r12p2.21-school.ru>
2018-12-06 14:39:37 +0300

    qwdqwd

---
+++ List/struct.h
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   struct.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mwuckert <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/12/05 16:26:28 by mwuckert          #+#    #+#             */
+/*   Updated: 2018/12/06 14:31:54 by mwuckert         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef STRUCT_H
+# define STRUCT_H
+
+# include <string.h>
+
+struct				people
+{
+	char			*name;
+	struct people	*junior;
+}					random_people;
+
+typedef struct people People;
+
+typedef struct		s_list
+{
+	void			*content;
+	size_t			content_size;
+	struct s_list	*next;
+}					t_list;
+
+#endif
