03f7995770bff155330de2f52f4e0ee5e12db786
Marjorie Wuckert <mwuckert@e3r2p2.21-school.ru>
2018-12-07 10:57:18 +0300

    hello world!

--- List/main.c
+++ List/main.c
@@ -6,7 +6,7 @@
 /*   By: mwuckert <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/06 13:44:19 by mwuckert          #+#    #+#             */
-/*   Updated: 2018/12/06 14:31:49 by mwuckert         ###   ########.fr       */
+/*   Updated: 2018/12/06 16:39:20 by mwuckert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,6 @@
 
 int		main(void)
 {
-	char *str = "Hello";
-	struct s_list list;
-	list.content = str;
-	ft_putstr();
+	
 	return (0);
 }
