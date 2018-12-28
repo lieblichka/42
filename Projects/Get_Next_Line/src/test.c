b1e2c3654f4c6d223eb33c0549a310f4bb611ba0
Marjorie Wuckert <mwuckert@e3r2p9.21-school.ru>
2018-12-28 22:48:13 +0300

    qwdqwd

---
+++ Projects/Get_Next_Line/src/test.c
@@ -0,0 +1,72 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   test.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mwuckert <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/12/25 17:51:40 by mwuckert          #+#    #+#             */
+/*   Updated: 2018/12/28 22:37:24 by mwuckert         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <fcntl.h>
+#include <unistd.h>
+
+#define BUF_SIZE 1
+
+int		readf(const int fd, char **line)
+{
+	static char *lst;
+	char *src;
+	char *addr;
+
+	if (!(*line = ft_strnew(1)))
+		return (-1);
+	else if (!lst && !(lst = ft_strnew(1)))
+		return (-1);
+	while (!ft_strrchr(*line, '\n') && read(fd, src = ft_strnew(BUF_SIZE), BUF_SIZE) > 0)
+	{
+		addr = *line;
+ 		if (!(*line = ft_strjoin(*line, src)))
+			return (-1);
+		ft_memdel((void**)&addr);
+		ft_memdel((void**)&src);
+	}
+	if (ft_strrchr(*line, '\n'))
+	{
+		addr = *line;
+		lst = ft_strdup(*line + ft_strlenc(*line, '\n'));
+		*line = ft_strncpy(ft_strnew(ft_strlenc(*line, '\n')),
+				*line, ft_strlenc(*line, '\n'));
+		ft_memdel((void**)&addr);
+		addr = *line;
+		*line = ft_strjoin(lst, *line);
+		ft_memdel((void**)&addr);
+		return (1);
+	}
+	return (0);
+}
+
+int		main(void)
+{
+	char	*line;
+	char	*addr;
+	int		read;
+	int		fd;
+	int		fd2;
+
+	read  = 1;
+	line = ft_strnew(1);
+	addr = line;
+	fd = open("files/42", O_RDONLY);
+	fd2 = open("files/42", O_RDONLY);	
+	while (read)
+	{
+		read = readf(fd, &line);
+		ft_putstr(line);
+	}
+	ft_memdel((void**)&addr);
+	return (0);
+}
