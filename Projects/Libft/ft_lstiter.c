512ca6eb4973e09b49f60d1565a885e79378a5e2
Marjorie Wuckert <mwuckert@e3r2p9.21-school.ru>
2018-12-28 22:48:45 +0300

    qwdqwd

--- Projects/Libft/ft_lstiter.c
+++ Projects/Libft/ft_lstiter.c
@@ -6,7 +6,7 @@
 /*   By: mwuckert <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/09 20:54:59 by mwuckert          #+#    #+#             */
-/*   Updated: 2018/12/10 10:43:58 by mwuckert         ###   ########.fr       */
+/*   Updated: 2018/12/25 16:05:45 by mwuckert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,8 @@ void	ft_lstiter(t_list *lst, void (*f)(t_list *elem))
 {
 	if (lst && f)
 	{
-		if (lst->next)
-			ft_lstiter(lst->next, f);
+		if ((*lst).next)
+			ft_lstiter((*lst).next, f);
 		f(lst);
 	}
 }
