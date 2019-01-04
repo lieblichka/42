224069cbc783cf26b64e7fbb2fd7467b81c9c612
nesteatea <nesteatea12@mail.ru>
2019-01-04 21:31:15 +0300

    push

--- Myscript/alias.sh
+++ Myscript/alias.sh
@@ -1,10 +1,11 @@
 # mwuckert config :)
 export PATH="$PATH:$HOME/.rvm/bin"
-export PS1="[%n% ]: "
+export PS1="[\u]: "
 cd ~/Desktop/
 alias p="pwd | rev | cut -d / -f 1 | rev"
 alias c=clear
 alias l="ls -1"
 alias desktop="cd ~/Desktop"
 alias downloads="cd ~/Downloads"
+alias battery="acpi | cut -d , -f 2 | tr -d ' '"
 clear
