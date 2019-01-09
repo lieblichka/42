e86e381442ffc8732f91e8c714123ecc162243c0
Marjorie Wuckert <mwuckert@e4r12p2.21-school.ru>
2019-01-09 15:42:39 +0300

    push

--- Projects/Get_Next_Line/get_next_line.h
+++ Projects/Get_Next_Line/get_next_line.h
@@ -6,17 +6,17 @@
 /*   By: mwuckert <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/13 19:54:16 by mwuckert          #+#    #+#             */
-/*   Updated: 2019/01/07 17:02:00 by mwuckert         ###   ########.fr       */
+/*   Updated: 2019/01/09 15:40:15 by mwuckert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# define BUFF_SIZE 1
+# define BUFF_SIZE 400
 
+# include <stdio.h>
 # include <unistd.h>
-# include <fcntl.h>
 # include "libft/libft.h"
 
 int	get_next_line(const int fd, char **line);
