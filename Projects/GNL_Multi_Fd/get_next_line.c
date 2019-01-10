caac3a5afbffd4389ad1f87f721028ad8e8d3189
Marjorie Wuckert <mwuckert@e4r12p2.21-school.ru>
2019-01-10 15:46:59 +0300

    push

--- Projects/GNL_Multi_Fd/get_next_line.c
+++ Projects/GNL_Multi_Fd/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: mwuckert <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/07 13:05:53 by mwuckert          #+#    #+#             */
-/*   Updated: 2019/01/09 18:53:14 by mwuckert         ###   ########.fr       */
+/*   Updated: 2019/01/10 15:46:27 by mwuckert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,14 +33,14 @@ int		readf(const int fd, char **lst)
 
 int		get_next_line(const int fd, char **line)
 {
-	t_list *lst;
+	static char *lst;
 	char		*addr;
 
-	if (!lst && !(lst = ft_lstnew((void*)ft_strnew(1), 1)))
+	if (!lst && !(lst = ft_strnew(1)))
 		return (-1);
 	if (!readf(fd, &lst))
 	{
-		ft_lstdel(&lst, &ft_lstdelcontent);
+		ft_memdel((void**)&lst);
 		return (-1);
 	}
 	if (ft_strlenc(lst, '\n') || ft_strrchr(lst, '\n'))
--- Projects/GNL_Multi_Fd/get_next_line.h
+++ Projects/GNL_Multi_Fd/get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: mwuckert <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/13 19:54:16 by mwuckert          #+#    #+#             */
-/*   Updated: 2019/01/09 18:53:12 by mwuckert         ###   ########.fr       */
+/*   Updated: 2019/01/10 13:04:20 by mwuckert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 # define GET_NEXT_LINE_H
 
 # define BUFF_SIZE 10000
+# define LST_BUF (*lst).content
 
 # include <unistd.h>
 # include "libft/libft.h"
