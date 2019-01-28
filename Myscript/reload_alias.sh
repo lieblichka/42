e60b222fda045928072608d0ea64465969be6e17
mwuckert <mwuckert@gmail.com>
2019-01-28 14:06:39 +0300

    qwdqwdq

--- Myscript/reload_alias.sh
+++ Myscript/reload_alias.sh
@@ -2,5 +2,5 @@ sleep 2
 echo "found " | tr -d "\n"
 find ~/ -maxdepth 1 -type f -name ".bashrc" -print -delete | rev | cut -d '/' -f 1 | rev 
 sleep 2
-cp ~/Desktop/42/Myscript/alias.sh  ~/.bashrc
+cp ~/Desktop/42/Myscript/file_sh/alias.sh  ~/.bashrc
 echo "reload .bashrc"
