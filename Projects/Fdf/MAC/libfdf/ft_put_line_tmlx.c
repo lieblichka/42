87d54033c813e4bedc590545dcc756b9b88d899b
Marjorie Wuckert <mwuckert@e4r11p3.21-school.ru>
2019-01-30 01:22:29 +0300

    wewef

--- Projects/Fdf/MAC/libfdf/ft_put_line_tmlx.c
+++ Projects/Fdf/MAC/libfdf/ft_put_line_tmlx.c
@@ -6,7 +6,7 @@
 /*   By: mwuckert <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/28 23:28:54 by mwuckert          #+#    #+#             */
-/*   Updated: 2019/01/30 01:19:27 by mwuckert         ###   ########.fr       */
+/*   Updated: 2019/01/30 01:21:17 by mwuckert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,8 +19,8 @@ void		ft_put_line_tmlx(t_mlx *m, int x, int y, int color)
 
 	straight.delta_x = x - (*m).x < 0 ? (x - (*m).x) * -1 : x - (*m).x;
 	straight.delta_y = y - (*m).y < 0 ? (y - (*m).y) * -1 : y - (*m).y;
-	straight.sign_x =  (*m).x < x ? 1 : -1;
-	straight.sign_y =  (*m).y < y ? 1 : -1;
+	straight.sign_x = (*m).x < x ? 1 : -1;
+	straight.sign_y = (*m).y < y ? 1 : -1;
 	straight.delta_xy = straight.delta_x - straight.delta_y;
 	ft_pixel_put_tmlx(m, x, y, color);
 	while ((*m).x != x || (*m).y != y)
