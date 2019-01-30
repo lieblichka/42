340bc75b4ba70b0b5d964f5fdb2cafcfe42347d5
Marjorie Wuckert <mwuckert@e4r11p3.21-school.ru>
2019-01-30 03:00:24 +0300

    qwdqwd

--- Projects/Fdf/MAC/libfdf/ft_read_fdf_file.c
+++ Projects/Fdf/MAC/libfdf/ft_read_fdf_file.c
@@ -1,26 +1,29 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*   ft_read_fdf_file.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mwuckert <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/11/30 16:24:10 by mwuckert          #+#    #+#             */
-/*   Updated: 2019/01/30 01:43:51 by mwuckert         ###   ########.fr       */
+/*   Created: 2019/01/30 01:34:09 by mwuckert          #+#    #+#             */
+/*   Updated: 2019/01/30 01:43:26 by mwuckert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
 #include "libft.h"
-#include <stdlib.h>
 
-char	*ft_strjoin(char const *s1, char const *s2)
+char	*ft_read_fdf_file(const int fd)
 {
-	char *s;
+	char *map;
+	char *buf;
+	char *addr;
 
-	if (!s1 || !s2 ||
-			!(s = (char*)(ft_memalloc(ft_strlen(s1) + ft_strlen(s2) + 1))))
-		return (0);
-	ft_strcpy(s, s1);
-	ft_strcat(s, s2);
-	return (s);
+	while (get_next_line(fd, &buf) > 0)
+	{
+		addr = map;
+		map = ft_strjoin(map, buf);
+		ft_memdel((void**)&addr);
+	}
+	return (map);
 }
