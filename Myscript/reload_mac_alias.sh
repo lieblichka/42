1f0831df1927cc6f098f0b98d77c855fa23d443a
Marjorie Wuckert <mwuckert@at-p8.21-school.ru>
2019-07-14 14:02:32 +0300

    delete all

--- Myscript/reload_mac_alias.sh
+++
@@ -1,6 +0,0 @@
-sleep 2
-echo "found " | tr -d "\n"
-find ~/ -maxdepth 1 -type f -name ".zshrc" -print -delete | rev | cut -d '/' -f 1 | rev 
-sleep 2
-cp ~/Desktop/42/Myscript/file_sh/mac_alias.sh  ~/.zshrc
-echo "reload .zshrc"
