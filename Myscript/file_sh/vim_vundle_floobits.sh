93d08714cd155b348189629f2263b355b3f48242
Marjorie Wuckert <mwuckert@oa-j5.21-school.ru>
2019-07-18 17:10:58 +0300

    push

---
+++ Myscript/file_sh/vim_vundle_floobits.sh
@@ -0,0 +1,23 @@
+set nocompatible			" be iMproved, required
+filetype off				" required
+
+set rtp+=~/.vim/bundle/Vundle.vim
+call vundle#begin()
+Plugin 'VundleVim/Vundle.vim'
+
+Bundle 'Floobits/floobits-vim'
+
+call vundle#end()
+
+set exrc	" - искать дополнительный .vimrc в текущей директории "
+set secure	" - запрет внешним .vimrc писать что-либо в файлы и "
+			" - выполнять команды шелла "
+
+set tabstop=4
+set smartindent " - автоматически ставит отступы в нужных местах "
+
+syntax on
+colorscheme pablo
+
+" ctrl+n - автодополнение "
+" %s/text/newtext/ - replace text in file "
