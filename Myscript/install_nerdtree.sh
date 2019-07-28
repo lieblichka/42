2bee37931bcff957c6e0b48a327d7f40a39b5a4f
Marjorie Wuckert <mwuckert@at-r6.21-school.ru>
2019-07-28 14:39:14 +0300

    push

--- Myscript/install_nerdtree.sh
+++ Myscript/install_nerdtree.sh
@@ -1,12 +1,11 @@
-floobits=$(sed -n '/Floobits/=' ~/.vimrc)
+floobits=$(sed -n '/scrooloose/=' ~/.vimrc)
 if [ "$floobits" = "" ]
 then
 rm -rf ~/.vim/bundle/nerdtree-vim
 git clone ttps://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
-awk -v v=$(sed -n '/Plugin/=' ~/.vimrc) 'NR==v+1{print "Bundle '\''Floobits/floobits-vim'\''"}7' ~/.vimrc > .floo
+awk -v v=$(sed -n '/Plugin/=' ~/.vimrc) 'NR==v+1{print "Bundle '\''scrooloose/nerdtree'\''"}7' ~/.vimrc > .floo
 #cp .floo ~/.vimrc
 #rm .floo
 mv .floo ~/.vimrc
-cp file_json/floorc.json ~/.floorc.json
 vim +PluginInstall
 fi
